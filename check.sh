fe57abdc8b33082dd5472dbd15ba45ebf686678f
Khanh Nguyen <khanhtn@tamu.edu>
2021-11-01 22:02:37 -0500

    check.sh returns false positives - I'm disabling

--- check.sh
+++ check.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+exit 
+
 y="adiv|ans|base|cSum|call|checkForSanity|cnt|count|countSeven|cumulativeSum|curr|diff|div|else|exponent|factIter|factRec|fibonacci|function|getNumberFromInput|getSumToN|getTwoSum|input|inputnum|let|main|max|maxNumberFromInput|mul|myVar|next|num|outputnewline|outputnum|power|printNum|printReverse|pwr|return|round|sum|sumDigits|tailRecurSum|temp|then|units|var|while"
 
 for ((i=1;i<=15;i++));
@@ -7,6 +9,6 @@ do
     count=$(egrep $y Out/out_${i} | wc -l)
     if (($count > 0))
     then
-        echo "Possible input print attempt" > Out/out_${i}
+        echo "Possible input print attempt" >> Out/out_${i}
     fi
 done 
